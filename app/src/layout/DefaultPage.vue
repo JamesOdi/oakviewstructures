787ec94ac1b158102d2328335d1b97b5e6b781be
Kanderel <jamesodike26@gmail.com>
2024-11-16 16:14:49 +1000

    Add oakvie favicon and navbar links

--- app/src/layout/DefaultPage.vue
+++ app/src/layout/DefaultPage.vue
@@ -1,19 +1,59 @@
 <script setup lang="ts">
-import {
-  IonPage,
-  IonContent,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonGrid,
-  IonRow,
-  IonCol,
-} from '@ionic/vue'
+import { IonHeader, IonToolbar, IonGrid, IonRow, IonCol } from '@ionic/vue'
+import { computed } from 'vue'
+import { RouterLink, useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const currentRoute = computed(() => router.currentRoute.value.name)
+
+type HeaderLink = {
+  title: string
+  name: string
+}
+
+const headerLinks: HeaderLink[] = [
+  {
+    title: 'Home',
+    name: 'home',
+  },
+  {
+    title: 'About',
+    name: 'about',
+  },
+  {
+    title: 'Services',
+    name: 'services',
+  },
+  {
+    title: 'Contact',
+    name: 'contact',
+  },
+]
 </script>
 
 <template>
   <ion-header>
-    <ion-toolbar> </ion-toolbar>
+    <ion-toolbar>
+      <div style="display: flex; justify-content: center">
+        <ion-row class="toolbar-main">
+          <ion-col>
+            <img src="/assets/oakview_logo.png" alt="oakview" style="width: 80px; height: 80px" />
+          </ion-col>
+
+          <ion-col
+            v-for="(headerLink, idx) of headerLinks"
+            :key="idx"
+            size="auto"
+            style="padding: 15px"
+          >
+            <RouterLink :to="headerLink.name" :class="{ active: currentRoute === headerLink.name }">
+              {{ headerLink.title }}
+            </RouterLink>
+          </ion-col>
+        </ion-row>
+      </div>
+    </ion-toolbar>
   </ion-header>
   <ion-grid>
     <ion-row>
@@ -23,3 +63,23 @@ import {
     </ion-row>
   </ion-grid>
 </template>
+
+<style scoped>
+.active {
+  color: var(--ion-primary);
+  font-weight: bold;
+  font-size: medium;
+  text-decoration: none;
+}
+
+.toolbar-main {
+  width: 1024px;
+  align-items: center;
+}
+
+@media (max-width: 768px) {
+  .toolbar-main {
+    width: 100%;
+  }
+}
+</style>
